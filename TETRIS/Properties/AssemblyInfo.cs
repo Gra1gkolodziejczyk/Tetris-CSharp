de06534b45686b7b03d17eb6bf134830e94231ef
Graig KOLODZIEJCZYK <graig.kolodziejczyk@ynov.com>
2022-02-06 23:58:22 +0100

    all files

---
+++ TETRIS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales sur un assemblage sont contrôlées par les éléments suivants
+// ensemble d'attributs. Modifiez ces valeurs d'attribut pour modifier les informations
+// associé à un assemblage.
+[assembly: AssemblyTitle("Tetris")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tetris")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Définir ComVisible sur false rend les types de cet assembly non visibles
+// aux composants COM. Si vous avez besoin d'accéder à un type dans cet assembly à partir de
+// COM, définissez l'attribut ComVisible sur true sur ce type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("055acab1-728f-41a4-9541-e1af3cba8d66")]
+
+// Les informations de version d'un assembly se composent des quatre valeurs suivantes :
+//
+// Version majeure
+// Version mineure
+// Numéro de construction
+// Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les numéros de version et de révision
+// en utilisant le '*' comme indiqué ci-dessous :
+// [assemblage : AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TETRIS/ShapesHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+
+namespace Tetris
+{
+    static class ShapesHandler
+    {
+        private static Shape[] shapesArray;
+        static ShapesHandler()
+        {
+            shapesArray = new Shape[]
+                {
+                    new Shape {
+                        Width = 2,
+                        Height = 2,
+                        Position = new int[,]
+                        {
+                            { 1, 1 },
+                            { 1, 1 }
+                        },
+                        Color = Brushes.Pink,
+                    },
+                    new Shape {
+                        Width = 1,
+                        Height = 4,
+                        Position = new int[,]
+                        {
+                            { 1 },
+                            { 1 },
+                            { 1 },
+                            { 1 }
+                        },
+                        Color = Brushes.Plum,
+                    },
+                    new Shape {
+                        Width = 3,
+                        Height = 2,
+                        Position = new int[,]
+                        {
+                            { 0, 1, 0 },
+                            { 1, 1, 1 }
+                        },
+                        Color = Brushes.HotPink,
+                    },
+                    new Shape {
+                        Width = 3,
+                        Height = 2,
+                        Position = new int[,]
+                        {
+                            { 0, 0, 1 },
+                            { 1, 1, 1 }
+                        },
+                        Color = Brushes.PaleVioletRed,
+                    },
+                    new Shape {
+                        Width = 3,
+                        Height = 2,
+                        Position = new int[,]
+                        {
+                            { 1, 0, 0 },
+                            { 1, 1, 1 }
+                        },
+                        Color = Brushes.CornflowerBlue,
+                    },
+                    new Shape {
+                        Width = 3,
+                        Height = 2,
+                        Position = new int[,]
+                        {
+                            { 1, 1, 0 },
+                            { 0, 1, 1 }
+                        },
+                        Color = Brushes.Crimson,
+                    },
+                    new Shape {
+                        Width = 3,
+                        Height = 2,
+                        Position = new int[,]
+                        {
+                            { 0, 1, 1 },
+                            { 1, 1, 0 }
+                        },
+                        Color = Brushes.RoyalBlue,
+                    }
+                };
+        }
+        
+        public static Shape GetRandomShape()
+        {
+            var shape = shapesArray[new Random().Next(shapesArray.Length)];
+
+            return shape;
+        }
+    }
+}
